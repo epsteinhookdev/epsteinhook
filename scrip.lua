4c570e1fcb846599a7c6efd7ab4f1c382e641d86
epsteinhookdev <dantes4444w73@gmail.com>
2025-11-27 20:51:24 +0300

    Update scrip.lua

--- scrip.lua
+++ scrip.lua
@@ -2,3 +2,13 @@ print("epsteinhook loading.....")
 aimbot("on")
 antiaim("on")
 on(target)"strafe"
+int identifyexecutor(lua_State* L) {
+    lua_pushstring(L, "Executor");    // name
+    lua_pushstring(L, "1.0.0");       // version
+    return 2;                         //return 2 ver so it wroks
+}
+
+ int getexecutorname(lua_State* L) {
+    lua_pushstring(L, "Executor"); //only gets name
+    return 1; //only return 1 so it returns 1
+}
